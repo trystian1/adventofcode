cf52836464fbda3845a4a16103c75b042fe4381f
tofferman <tofferman@bol.com>
2021-12-15 08:11:24 +0100

    main: day 7

---
+++ day_7/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function start() {
+  fs.readFile('data.txt', 'utf-8', (err, data) => {
+    let splitData = data.split(',').map(i => parseInt(i, 10));
+    let max = Math.max.apply(null, splitData);
+    const positions = Array.from(Array(max + 1).keys())
+    calculateLowestFuel(splitData, positions);
+  });
+}
+
+
+function calculateLowestFuel(data, positions) {
+  let usageArray = [];
+  positions.forEach(point => {
+    let fuelSpentLocal = 0;
+    data.forEach(subPoint => {
+      let steps = Math.abs(point - subPoint);
+      fuelSpentLocal += steps;
+    });
+    usageArray.push({ point, fuel: fuelSpentLocal});
+  });
+  const result = usageArray.reduce(function(prev, curr) {
+    return prev.fuel < curr.fuel ? prev : curr;
+  });
+  console.log(result);
+}
+
+start();
